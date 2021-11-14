338dfa6d4a56533fdd04656085846f250108431e
Davi Holanda <daviht7@gmail.com>
2021-11-13 23:19:16 -0300

    adicao do devtools no pom e dominio Categoria

--- src/main/java/com/daviholanda/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/daviholanda/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.daviholanda.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.daviholanda.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listart() {
-		return "rest está funcionando";
+	public List<Categoria> listart() {
+		
+		Categoria categoria = new Categoria(1,"Informática");
+
+		Categoria categoria2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(categoria);
+
+		categorias.add(categoria2);
+		
+		return categorias;
 	}
 	
 }
