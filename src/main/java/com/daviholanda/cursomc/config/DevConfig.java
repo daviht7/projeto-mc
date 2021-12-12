4f955c6731e0a9921ba0488f02827a1a369153c4
Davi Holanda <daviht7@gmail.com>
2021-12-12 18:21:11 -0300

    Implementando SmtpEmailServico com servidor do google

--- src/main/java/com/daviholanda/cursomc/config/DevConfig.java
+++ src/main/java/com/daviholanda/cursomc/config/DevConfig.java
@@ -1,12 +1,16 @@
 package com.daviholanda.cursomc.config;
 
 import com.daviholanda.cursomc.service.DBService;
+import com.daviholanda.cursomc.service.EmailService;
+import com.daviholanda.cursomc.service.MockEmailService;
+import com.daviholanda.cursomc.service.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import javax.validation.constraints.Email;
 import java.text.ParseException;
 
 @Configuration
@@ -28,6 +32,11 @@ public class DevConfig {
 
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService smtpEmailService() {
+        return new SmtpEmailService();
     }
 
 }
---
+++ src/main/java/com/daviholanda/cursomc/service/SmtpEmailService.java
@@ -0,0 +1,25 @@
+package com.daviholanda.cursomc.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class SmtpEmailService extends AbstractEmailService {
+
+    @Autowired
+    private MailSender mailSender;
+
+    private static final Logger logger = LoggerFactory.getLogger(SmtpEmailService.class);
+
+    @Override
+    public void sendEmail(SimpleMailMessage msg) {
+
+        logger.info("Simulando envio de email...");
+        logger.info(msg.toString());
+        mailSender.send(msg);
+        logger.info("Email enviado.");
+
+    }
+}
