4b7c7c326d75af583acb9338cd31a07fbfc9a648
Davi Holanda <daviht7@gmail.com>
2021-12-20 20:38:41 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/daviholanda/cursomc/domain/Cliente.java
+++ src/main/java/com/daviholanda/cursomc/domain/Cliente.java
@@ -43,6 +43,8 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "PERFIS")
     private Set<Integer> perfis = new HashSet<>();
 
+    private String imageUrl;
+
     public Cliente() {
         addPerfil(Perfil.CLIENTE);
     }
@@ -136,6 +138,14 @@ public class Cliente implements Serializable {
 
     public void addPerfil(Perfil perfil) {
         this.perfis.add(perfil.getCod());
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     @Override
--- src/main/java/com/daviholanda/cursomc/service/ClienteService.java
+++ src/main/java/com/daviholanda/cursomc/service/ClienteService.java
@@ -126,7 +126,20 @@ public class ClienteService {
 
     public URI uploadProfilePicture(MultipartFile multipartFile) {
 
-        return s3Service.uploadFile(multipartFile);
+        UserSS user = UserService.authenticated();
+
+        if(user == null) {
+            throw new AuthorizationException("Acesso negado");
+        }
+
+        URI uri = s3Service.uploadFile(multipartFile);
+
+        Cliente cli = clienteRepository.findById(user.getId().longValue()).get();
+        cli.setImageUrl(uri.toString());
+
+        clienteRepository.save(cli);
+
+        return uri;
 
     }
 
